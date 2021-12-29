c1e6912c904d9414d8c3ce0735f6a1d69085d2cc
Kamil Socha <magister602@gmail.com>
2021-12-29 16:22:25 +0100

    + server scripts

---
+++ Server Scripts/scripts.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using App.CustomConsole;
+
+namespace App.ServerScripts
+{
+    public class User
+    {
+        public int id;
+        public string name;
+    }
+    public static class Scripts
+    {
+        public static List<User> users = new List<User>();
+
+        public static User GetUser(int id)
+        {
+            foreach(User user in users)
+            {
+                if(user.id == id)
+                    return user;
+            }
+
+            return null;
+        }
+
+        public static void OnConnect(int id)
+        {
+            MyConsole.Info($"User [ID:{ id }] has connected.");
+            User user = new User();
+            user.id = id;
+            users.Add(new User());
+        }
+
+        public static void OnChangeName(int id, string newName, string oldName)
+        {
+            GetUser(id).name = newName;
+            MyConsole.Info($"User [ID:{ id }] has changed name to { newName }");
+        }
+
+        public static void OnSendMessage(int id, string message)
+        {
+            MyConsole.PlayerMessage(GetUser(id).name + "> ", message);
+        }
+
+        public static void OnCommand(int id, string cmd, string args)
+        {
+            switch(cmd)
+            {
+                case "test":
+                    MyConsole.WriteLine($"{GetUser(id).name} is testing: { args }");
+                    break;
+            }
+        }
+    }
+}
