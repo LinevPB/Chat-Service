43acacf76d154f8d881e5004e19b48743ae7530d
Kamil Socha <magister602@gmail.com>
2021-12-29 17:10:40 +0100

    server arg ip route

--- Server/Main.cs
+++ Server/Main.cs
@@ -5,13 +5,17 @@ namespace Server
 {
     public static class Server
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
             Console.Clear();
             MyConsole.WriteLine(ConsoleColor.Blue, "Chat server application");
             MyConsole.NewLine();
 
-            SocketController controller = new SocketController(28970);
+            string ip = "default";
+            if (args.Length > 0)
+                ip = args[0];
+
+            SocketController controller = new SocketController(28970, ip);
             controller.Start();
         }
     }
